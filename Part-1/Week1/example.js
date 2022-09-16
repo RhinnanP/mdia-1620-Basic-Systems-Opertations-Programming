ccb5c5ef50d71de8f8c10de2a2336d4955f0d020
Rhinnan Payne <rpayne16@my.bcit.ca>
2022-09-16 12:15:59 -0700

    adding more code

--- Part-1/Week1/example.js
+++ Part-1/Week1/example.js
@@ -1,2 +1,39 @@
 "testing"
 var car;
+
+//camelCase
+var bugsCollected = 4;
+
+//PascalCase
+var BugsCollected = 4;
+
+//Snacke_Case
+var Bugs_Collected = 4;
+
+var electronicsBox = "cpu";
+
+//Boolean
+
+var proceed = true;
+var i = 0;
+
+while(proceed) {
+    i++;
+    proceed = false;
+}
+
+console.log(i);
+
+// Data Type - Object
+/**
+ *  
+ * 
+ */
+
+//Create an object
+var grannysmith = {
+    type: "green",
+    originLocation: "australia",
+    originDate: 1868,
+};
+console.log(grannysmith);
